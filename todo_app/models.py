ad5a1ce153a3d16810243988bf7fb01865a24055
Abdullah OZER <abdullahozer11@hotmail.com>
2022-10-17 00:24:45 +0200

    new data models are added to the models.py file

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,3 +1,24 @@
+
 from django.db import models
+from django.utils import timezone
+
+
+def one_week_hence():
+    return timezone.now() + timezone.timedelta(days=7)
 
 # Create your models here.
+class ToDoList(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.title
+
+class ToDoItem(models.Model):
+    title = models.CharField(max_length=100)
+    created_date = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(null=True, default=None)
+    due_date = models.DateTimeField(default=one_week_hence)
+    todo_list = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
