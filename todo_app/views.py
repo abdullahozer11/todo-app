05bf1ad0569f3fdaa0128a0ecce093d4bac7b2e3
Abdullah OZER <abdullahozer11@hotmail.com>
2022-10-22 18:25:06 +0200

    redundant imports are removed

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,6 +1,6 @@
 # Create your views here.
-from django.urls import reverse, reverse_lazy
-from django.views.generic import ListView, DeleteView, FormView, CreateView, UpdateView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DeleteView, CreateView, UpdateView
 
 from todo_app.forms import ListForm, ItemForm
 from todo_app.models import ToDoList, ToDoItem
