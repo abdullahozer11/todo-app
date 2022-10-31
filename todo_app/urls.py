11703f686c467c7ad3259bd3202a469352b1b7a8
Abdullah OZER <abdullahozer11@hotmail.com>
2022-10-31 23:14:36 +0100

    if debug is false fix media url request handling and modify allowed hosts

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -20,3 +20,5 @@ urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
     path("profile/<pk>", ProfileView.as_view(), name="profile"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if not settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
